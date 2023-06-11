1029a3d6b348c65668bf2b81f644b09c70e8a72c
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2023-06-11 11:58:58 +0200

    Add create_user page and test

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,6 +16,23 @@ class UserAdmin(BaseUserAdmin):
         (_("Important_dates"), {"fields": ("last_login",)}),
     )
     readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
